c0b2e40731baef66c82cd5c7a20cf94b552791c5
connected <connected@connecteds-MacBook-Pro.local>
2019-06-24 14:43:50 +0200

    Video quality dims and ind vars are saved to the desktop acc to their list order

--- Video Quality Comparison.R
+++ Video Quality Comparison.R
@@ -1,11 +1,26 @@
 require(caTools)
 require(ggplot2)
+require(gridExtra)
 
 dataset <- read.csv('Dataset-G1.csv', sep = ';', stringsAsFactors = F, dec = ',')
 
 ## Functions
 rsq <- function(x, y) cor(x, y)^2
 rmse <- function(x, y) sqrt(mean((x-y)^2))
+create_dens <- function(variab, ds) {
+  vr <- ds[[variab]]
+  bw <- (max(vr)-min(vr))/length(vr)^(1/3)
+  # print(bw)
+  print(sqrt(length(vr)))
+  dens <- ggplot(ds, aes_string(x=variab))+  
+    geom_histogram(aes(y=..density..), position = 'identity', alpha=0.6) + 
+    # geom_histogram(aes(y=..density..), position = 'identity', alpha=0.6, binwidth = bw) + 
+    geom_density(alpha=.4, fill='red') +
+    geom_vline(aes(xintercept=mean(ds[,variab])), color='blue', lty=2)+
+    scale_color_brewer(palette = 'Dark2') +
+    theme_minimal()
+  print(dens)
+}
 
 # Split the Data Set into train and test parts
 set.seed(2019) # seed for the random number generator
@@ -107,39 +122,6 @@ rmse_both <-rmse(comp_both$actuals, comp_both$predicteds)
 
 
 # Visualising the data
-direct <- c(cor_ind, rmse_ind, rsq_ind)
-indirect <- c(cor_both, rmse_both, rsq_both)
-
-col.1 <- c(cor_ind, cor_both)
-col.2 <- c(rmse_ind, rmse_both)
-col.3 <- c(rsq_ind, rsq_both)
-
-matr <- matrix(c(col.1, col.2, col.3), nrow = 2, ncol = 3)
-colnames(matr) <- c("Correlation", "RMSE", "RSQ")
-rownames(matr) <- c("Direct", "Indirect")
-
-bp <- barplot(matr, beside = T, legend.text = rownames(matr), ylim = c(0, max(matr)+0.5), col = c('#77E3C4', '#FFAF7B'))
-
-# bp
-# legend(x=max(matr)-0.2, y=max(matr)-0.2, legend = rownames(matr))
-# text(x=bp, label = matr)
-
-cols <- c("Direct", "Indirect", "Var")
-row.1 <- c(cor_ind, cor_both, "Correlation")
-row.2 <- c(rmse_ind, rmse_both, "RMSE")
-row.3 <- c(rsq_ind, rsq_both, "RSQ")
-
-df <- t(matrix(c(row.1, row.2, row.3), nrow = 3, ncol = 3))
-colnames(df) <- cols
-df <- data.frame(df)
-bp <- ggplot(data = df,
-             aes(x=df$Var,
-                 y=df$Direct,
-                 fill=colnames(df)))+
-  geom_bar(stat = 'identity')
-bp
-
-## Ikinci deneme
 col.1 <- c("Direct", "Direct", "Direct", "Indirect", "Indirect", "Indirect")
 col.2 <- c(cor_ind, rsq_ind, rmse_ind, cor_both, rsq_both, rmse_both)
 col.3 <- c("Correlation", "RSQ", "RMSE", "Correlation", "RSQ", "RMSE")
@@ -147,19 +129,182 @@ col.3 <- c("Correlation", "RSQ", "RMSE", "Correlation", "RSQ", "RMSE")
 df1 <-  matrix(c(col.1,round(col.2, 2), col.3), nrow = 6, ncol = 3) 
 colnames(df1) <- c("Dir_Indir", "Val", "Type")
 df1 <- data.frame(df1)
+
 bp1 <- ggplot(data=df1,
               aes(x=df1$Type,
                   y=df1$Val,
-                  # y=as.factor(seq(0.0, 1.0, length.out = nrow(df1))),
-                  fill=df1$Dir_Indir))+
+                  # y=as.factor(seq(0, 1, length.out = nrow(df1))),
+                  fill=df1$Dir_Indir,
+                  group=df1$Dir_Indir))+
   geom_bar(stat='identity', position = position_dodge(), color='black')+
-  geom_text(aes(label=df1$Val), color='black', position = position_dodge(0.9), size=4.5)+
+  geom_text(aes(label=df1$Val), color='black', position = position_dodge2(1), size=4.5, vjust=-0.5)+
   theme_minimal() +
-  # coord_fixed(ratio = 0.2) +
+  labs(title = "Comparison of Two Techniques", x="Different Metrics", y="Values", fill="Techniques")+
   scale_fill_manual(values = c('#77E3C4', '#FFAF7B'))
 bp1
 
-ggsave('~/Desktop/cor_rmse_rsq.png')
+# Create density distributions of the independent vars
+vq_dens <- create_dens('VQ', dataset)
+
+frag_dens <- create_dens('VF', dataset)
+disc_dens <- create_dens('VD', dataset)
+uncl_dens <- create_dens('VU', dataset)
+
+br_dens <- create_dens('Bitrate', dataset)
+fr_dens <- create_dens('Framerate', dataset)
+res_dens <- create_dens('Resolution', dataset)
+br_dens
+vq_dims <- grid.arrange(frag_dens, disc_dens, uncl_dens)
+ind_vars <- grid.arrange(br_dens, fr_dens, res_dens)
+
+
+## Showing independent variables for the video quality
+# Showing the bitrate values for the list order
+ind_vars <- ggplot(dataset)+
+  theme(legend.position = 'none')+
+  labs(title='Independent Variable', x = "Order In The List")
+
+br_vals <- ind_vars +  
+  geom_point(aes(x=seq(1, length(Bitrate)), 
+                 y=Bitrate, 
+                 colour=cut(Bitrate, c(-Inf, 300, 1000, 2000, 5000, 10000, Inf))))
+br_vals
+
+# Showing the framerate values for the list order
+fr_vals <- ind_vars+
+  geom_point(aes(x=seq(1, length(Framerate)),
+                 y=Framerate,
+                 ))
+fr_vals
+
+# Showing the resolution values for the list order
+res_vals <- ind_vars+
+  geom_point(aes(x=seq(1, length(Resolution)),
+                 y=Resolution,
+                 colour=cut(Resolution, c(-Inf, 480, 720, 1080, Inf))))
+res_vals
+
+## Saving the Graphs to the desktop
+# Distributions
+# ggsave('~/Desktop/ind_vars.png', plot = ind_vars)
+ggsave('~/Desktop/vq_dims.png', plot = vq_dims)
+ggsave('~/Desktop/br.png', plot = br_dens)
+ggsave('~/Desktop/fr.png', plot = fr_dens)
+ggsave('~/Desktop/res.png', plot = res_dens)
+
+ggsave('~/Desktop/vq_dens.png', plot = vq_dens)
+ggsave('~/Desktop/frag_dens.png', plot = frag_dens)
+ggsave('~/Desktop/disc_dens.png', plot = disc_dens)
+ggsave('~/Desktop/uncl_dens.png', plot = uncl_dens)
+
+# Technique Comparison
+# All three included
+ggsave('~/Desktop/comp_of_techniques.png', plot = bp1, device = 'png')
+
+# Independent Variables acc to the list order
+ggsave('~/Desktop/br_vals.png', plot = br_vals)
+ggsave('~/Desktop/fr_vals.png', plot = fr_vals)
+ggsave('~/Desktop/res_vals.png', plot = res_vals)
+
+
+## Saving the models to the Desktop
+# sink('~/Desktop/model_equations.txt')
+# vid_qual_frag_disc_uncl
+# vid_qual_br_fr_res
+# frag_br_fr_res
+# disc_br_fr_res
+# uncl_br_fr_res
+# sink()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
